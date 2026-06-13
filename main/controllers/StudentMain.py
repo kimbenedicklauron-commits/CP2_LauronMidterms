1bbfbe9b44ff8f7ac5b5c69c821ba16c314ff908
Kimilangbhie <kimbenedicklauron@gmail.com>
2026-06-13 11:28:18 +0800

    Revisions for the MVC. Added meaningful validations for the controller. Initiating revisions for Tkinter transition to desktop application.

---
+++ main/controllers/StudentMain.py
@@ -0,0 +1,9 @@
+# Kim Benedick Lauron
+
+from StudentController import main
+
+if __name__ == "__main__":
+    print("=" * 50)
+    print("   WELCOME TO STUDENT INFORMATION SYSTEM")
+    print("=" * 50)
+    main()
